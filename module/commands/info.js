c8d7fba11ec5dceac7a0f23696ce0b712ea7247c
jakaria1234e <mrnur0566818859@gmail.com>
2025-02-08 20:32:31 +0300

    Update info.js

--- module/commands/info.js
+++ module/commands/info.js
@@ -39,17 +39,17 @@ const moment = require("moment-timezone");
 
 ⁂𝘽𝙤𝙩 𝙉𝙖𝙢𝙚: ${global.config.BOTNAME}
 ✡𝘽𝙤𝙩 𝙋𝙧𝙚𝙛𝙞𝙭: ${global.config.PREFIX}
-✫𝘾𝙤𝙙𝙚 𝙊𝙬𝙣𝙚𝗿🌼 :🔰𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐀𝐧𝐢𝐤🔰,
+✫𝘾𝙤𝙙𝙚 𝙊𝙬𝙣𝙚𝗿🌼 :🔰Jakaria🔰,
 
-✬𝐅𝐛 𝐋𝐢𝐧𝐤: https://www.facebook.com/LostFragmentX
-✬𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+601161213515
+✬𝐅𝐛 𝐋𝐢𝐧𝐤: https://www.facebook.com/jakaria59r?mibextid=ZbWKwL
+✬𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+966566818859
 
 ➳✴️𝙈𝘼𝙎𝙏𝙀𝙍 𝙊𝙁 𝘽𝙊𝙏'𝙎 𝙄𝙉𝙎𝙄𝘿𝙀✴️
 
-🔰𝘾𝙤𝙙𝙚 𝘼𝙙𝙢𝙞𝙣𝙨🔰: [𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐀𝐧𝐢𝐤]
+🔰𝘾𝙤𝙙𝙚 𝘼𝙙𝙢𝙞𝙣𝙨🔰: [Jakaria]
 
 ➳𝙑𝙚𝙧𝙨𝙞𝙤𝙣 ✨: 1.0.0
-✬𝐌𝐨𝐝𝐢𝐟𝐢𝐞𝐝 𝐁𝐲 : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐀𝐧𝐢𝐤 🔰
+✬𝐌𝐨𝐝𝐢𝐟𝐢𝐞𝐝 𝐁𝐲 : jakaria 🔰
 
 ➳𝘽𝙤𝙩 𝙞𝙨 𝙍𝙪𝙣𝙣𝙞𝙣𝙜⌚ ${hours} : ${minutes} : ${seconds}
 
